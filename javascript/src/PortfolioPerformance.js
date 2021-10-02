a0e04e05fb75d88143c115336b0ef28a55c74011
han.loo <nlhm5353@gmail.com>
2021-10-02 22:54:30 +0100

    submission

--- javascript/src/PortfolioPerformance.js
+++ javascript/src/PortfolioPerformance.js
@@ -16,5 +16,69 @@ const transactions = [
 ];
 
 export function getDailyPortfolioValues() {
-    return [];
+    var currentDate = new Date(2021, 8, 1);
+    const timezoneOffset = -currentDate.getTimezoneOffset() * 60 * 1000;
+    const dayAfterEndDate = new Date(2021, 8, 8);
+
+    currentDate.setTime(currentDate.getTime() + timezoneOffset);
+    dayAfterEndDate.setTime(dayAfterEndDate.getTime() + timezoneOffset);
+
+    const bitcoinValue = {};
+    const totalBitcoinAmount = {};
+    var bitcoinCounter = 0;
+
+    prices.forEach((priceDates) => {
+        const dateString = priceDates.effectiveDate.toISOString().split("T")[0];
+        bitcoinValue[dateString] = priceDates.price;
+    });
+
+    transactions.forEach((transactionDates) => {
+        const dateString = transactionDates.effectiveDate
+            .toISOString()
+            .split("T")[0];
+
+        bitcoinCounter += transactionDates.value;
+        totalBitcoinAmount[dateString] = bitcoinCounter;
+    });
+
+    const portfolioValue = [];
+    var lastAvailablePrice = undefined;
+    var lastAvailableBitcoinAmount = 0;
+
+    while (currentDate.getDate() != dayAfterEndDate.getDate()) {
+        const currentDateISOFormat = currentDate.toISOString().split("T")[0];
+        const dailyPortfolioValue = {};
+        const dailyPriceData = bitcoinValue[currentDateISOFormat];
+        const dailyBitcoinTotal = totalBitcoinAmount[currentDateISOFormat];
+
+        lastAvailablePrice =
+            dailyPriceData === undefined ? lastAvailablePrice : dailyPriceData;
+        lastAvailableBitcoinAmount =
+            dailyBitcoinTotal === undefined
+                ? lastAvailableBitcoinAmount
+                : dailyBitcoinTotal;
+
+        dailyPortfolioValue["effectiveDate"] = currentDateISOFormat;
+        dailyPortfolioValue["value"] =
+            lastAvailablePrice === undefined
+                ? "No previous bitcoin value available"
+                : parseFloat(
+                    (lastAvailablePrice * lastAvailableBitcoinAmount).toFixed(5)
+                );
+
+        portfolioValue.push(dailyPortfolioValue);
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    return portfolioValue;
 }
+
+// time complexity = O(n)
+
+/*
+Pros:
+- able to handle any start and end date range
+- able to support any UTC + timezones
+- additional check to prevent NaN output in situations where bitcoin transactions
+  exist earlier than first available bitcoin price
+*/
